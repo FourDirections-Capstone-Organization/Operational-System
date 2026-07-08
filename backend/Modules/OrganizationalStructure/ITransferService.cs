e3519c69559d39ded57b42e7757017bb9c747c87
IAmHaiel <mikhjnelo@gmail.com>
2026-07-08 15:46:13 +0800

    Added Interface for Transfer Service

---
+++ backend/Modules/OrganizationalStructure/ITransferService.cs
@@ -0,0 +1,9 @@
+﻿using Backend.Models;
+using Backend.Models.DTOs;
+
+namespace Backend.Modules.OrganizationalStructure;
+
+public interface ITransferService
+{
+    Task<ApiResponseDTO<bool>> TransferUserAsync(Guid userId, TransferUserDTO dto);
+}
