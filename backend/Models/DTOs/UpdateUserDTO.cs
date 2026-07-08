039a6abe1ce3d6f84a1b03c03f4c23966c8451b5
IAmHaiel <mikhjnelo@gmail.com>
2026-07-08 19:32:42 +0800

    Added UpdateUserDTO

---
+++ backend/Models/DTOs/UpdateUserDTO.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Backend.Models.DTOs;
+
+public class UpdateUserDTO
+{
+    [MaxLength(50)]
+    public string? FirstName { get; set; }
+
+    [MaxLength(50)]
+    public string? MiddleName { get; set; }
+
+    [MaxLength(50)]
+    public string? LastName { get; set; }
+
+    [MaxLength(20)]
+    public string? Suffix { get; set; }
+
+    [MaxLength(20)]
+    public string? ContactNumber { get; set; }
+
+    [MaxLength(100)]
+    [EmailAddress]
+    public string? Email { get; set; }
+}
