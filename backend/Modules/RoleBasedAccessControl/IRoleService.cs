86ae1d26a67d886f5079f19252db2d72bdafc727
IAmHaiel <mikhjnelo@gmail.com>
2026-07-09 21:19:21 +0800

    Added Interface for Role Service

---
+++ backend/Modules/RoleBasedAccessControl/IRoleService.cs
@@ -0,0 +1,13 @@
+﻿using Backend.Models;
+using Backend.Models.DTOs;
+using Backend.Models.Enums;
+
+namespace Backend.Modules.RoleBasedAccessControl;
+
+public interface IRoleService
+{
+    ApiResponseDTO<List<RoleResponseDTO>> GetAllRoles();
+    ApiResponseDTO<RoleResponseDTO> GetRoleByType(UserRole role);
+    Task<ApiResponseDTO<UserPermissionsDTO>> GetUserPermissions(Guid userId);
+    Task<ApiResponseDTO<bool>> UpdateUserRoleAsync(Guid userId, UpdateUserRoleDTO dto, Guid requestUserId);
+}
