59303d3784d93f9f1e919455d68f42ec4116b5bc
IAmHaiel <mikhjnelo@gmail.com>
2026-07-09 21:38:19 +0800

    Implemented the Get All Roles in Role Controller

---
+++ backend/Controllers/RoleController.cs
@@ -0,0 +1,28 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Backend.Models.DTOs;
+using Backend.Models.Enums;
+using Backend.Modules.RoleBasedAccessControl;
+
+namespace Backend.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+[Authorize]
+public class RoleController : ControllerBase
+{
+    private readonly IRoleService _roleService;
+
+    public RoleController(IRoleService roleService)
+    {
+        _roleService = roleService;
+    }
+
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        var result = _roleService.GetAllRoles();
+        return Ok(result);
+    }
+}
